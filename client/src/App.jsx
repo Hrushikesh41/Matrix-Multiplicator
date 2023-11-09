fe6a999ff718614448924cc56937a15879544ea3
Hrushikesh41 <hrushikesh.kok@gmail.com>
2023-11-09 12:22:32 +0530

    Initia; Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Matrix from './components/Matrix'
+
+function App() {
+
+  return (
+    <>
+      <Matrix />
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/Matrix.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Matrix = () => {
+  return (
+   <>
+    Matrix Multiplication
+   </>
+  )
+}
+
+export default Matrix
